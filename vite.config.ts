1f4196b855f12cdb2ab1d6532f8561899bc05e8c
Alexandr Kalinin <snurocher@gmail.com>
2025-02-07 10:38:15 +0300

    chore(vite.config.ts): fix env

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,33 @@
 /// <reference types="vitest" />
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
-export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './setup-tests.ts',
-    coverage: {
-      include: ['src/**/*.{ts,tsx}'],
-      exclude: [
-        '/dist/**',
-        '/*.config.{ts,js}',
-        'src/**/*.d.ts',
-        'src/**/index.ts',
-      ]
-    },
-  },
+export default defineConfig(({ mode }) => {
+  process.env = loadEnv(mode, process.cwd())
 
+  return {
+    base: process.env.VITE_BASE_URL || '/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './setup-tests.ts',
+      coverage: {
+        include: ['src/**/*.{ts,tsx}'],
+        exclude: [
+          '/dist/**',
+          '/*.config.{ts,js}',
+          'src/**/*.d.ts',
+          'src/**/index.ts',
+        ]
+      },
+    },
+
+  }
 })
